815a0b13b1ca65c31bf71b8ebc4575790e16ea2c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-01 12:21:37 -0500

    another test

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ var sampleRule = {
   "description": null
 };
 
+var sampleRule2 = {
+  "part": "filename",
+  "type": "regex",
+  "pattern": "\\A\\.?(bash_|zsh_|z)?history\\z",
+  "caption": "Shell command history file",
+  "description": null
+};
+
 function pythonToJavaScriptRegularExpression(pattern) {
   return pattern
     .replace('\\A', '^')
     .replace('\\z', '$');
 }
 
+/*
 var fixedPattern = pythonToJavaScriptRegularExpression(sampleRule.pattern);
 console.log('sample pattern', fixedPattern);
-
 var sampleRegex = new RegExp(fixedPattern);
 console.assert(sampleRegex.test('id_rsa'));
+*/
+
+var fixedPattern = pythonToJavaScriptRegularExpression(sampleRule2.pattern);
+console.log('sample pattern', fixedPattern);
+var sampleRegex = new RegExp(fixedPattern);
+console.assert(sampleRegex.test('.bash_history'));
+console.assert(sampleRegex.test('.zsh_history'));
+console.assert(sampleRegex.test('.zhistory'));
